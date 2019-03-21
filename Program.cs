136e134c0ada5a4ef27c01a529bedfc27b3bf92c
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-21 15:35:14 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circleprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Circle radius: ");
+            double valueCircle = double.Parse(Console.ReadLine());
+            double circleCircumference = 2 * Math.PI * valueCircle;
+            double circleArea = Math.PI * (valueCircle * valueCircle);
+            Console.WriteLine("Area: " + circleArea);
+            Console.WriteLine("Circumference: " + circleCircumference);
+            Console.Write("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
